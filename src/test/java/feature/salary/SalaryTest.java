9e6bd3dc3835c2baaea6fd57902dfa9f73c5db05
Pedro Meira <pv.ciria@gmail.com>
2017-05-10 22:53:49 -0300

    Adding another feature

--- src/test/java/feature/salary/SalaryTest.java
+++ src/test/java/feature/salary/SalaryTest.java
@@ -1,7 +1,11 @@
 package feature.salary;
 
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
 /**
  * Created by pvmeira on 10/05/17.
  */
+@RunWith(Cucumber.class)
 public class SalaryTest {
 }
